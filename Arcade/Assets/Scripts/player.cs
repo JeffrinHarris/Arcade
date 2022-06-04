1f9fb93097dd37c3ee760866d92369f2f89fba7a
Jeffrin Harris M <jeffrin.harris@gmail.com>
2022-06-05 01:36:56 +0530

    Player Jump

--- Arcade/Assets/Scripts/player.cs
+++ Arcade/Assets/Scripts/player.cs
@@ -10,23 +10,27 @@ public class player : MonoBehaviour
     public float speed;
     public Vector2 jumpHeight;
     public Animator animator;
+    private SpriteRenderer _renderer;
     void Start()
     {
         t = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
         float mx=Input.GetAxis("Horizontal");
         float gx=Input.GetAxis("Vertical");
 
-        animator.SetFloat("HorizontalAxis", mx);
+        animator.SetFloat("HorizontalAxis", Mathf.Abs(mx));
 
         if (mx > 0f) {
             rb.velocity = new Vector2 (mx * speed, rb.velocity.y);
+            _renderer.flipX = false;
         }
         else if (mx < 0f) {
             rb.velocity = new Vector2 (mx * speed, rb.velocity.y);
+            _renderer.flipX = true;
         } 
         else {
             rb.velocity = new Vector2 (0,rb.velocity.y);
@@ -34,7 +38,7 @@ public class player : MonoBehaviour
 
         // transform.Translate(speed * Time.deltaTime, 0f, 0f);  //makes player run
 
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))  //makes player jump
+        if (Input.GetKeyDown(KeyCode.Space))  //makes player jump
         {
             GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
         }
